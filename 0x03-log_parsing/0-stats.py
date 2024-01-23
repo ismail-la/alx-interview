b13845d0ce02aa45eb153ca07c5eca15299a3ea3
ismail-la <lahbariismail@gmail.com>
2024-01-24 00:52:10 +0100

    0x03. Log Parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,13 +2,18 @@
 '''Module for log parsing script.
 Write a script that reads stdin line by line and computes metrics:
 
--Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size> (if the format is not this one, the line must be skipped)
--After every 10 lines and/or a keyboard interruption (CTRL + C), print these statistics from the beginning:
+-Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+ <status code> <file size> (if the format is not this one,
+ the line must be skipped)
+-After every 10 lines and/or a keyboard interruption (CTRL + C),
+ print these statistics from the beginning:
     ->Total file size: File size: <total size>
-    ->where <total size> is the sum of all previous <file size> (see input format above)
+    ->where <total size> is the sum of all previous <file size>
+      (see input format above)
     ->Number of lines by status code:
       -possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-      -if a status code doesn’t appear or is not an integer, don’t print anything for this status code
+      -if a status code doesn’t appear or is not an integer,
+       don’t print anything for this status code
       -format: <status code>: <number>
       -status codes should be printed in ascending order
 
@@ -18,7 +23,7 @@ Write a script that reads stdin line by line and computes metrics:
 import sys
 
 codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
-                     '404': 0, '405': 0, '500': 0}
+              '404': 0, '405': 0, '500': 0}
 
 size_total = 0
 count = 0
@@ -44,9 +49,6 @@ try:
             for key, value in sorted(codes_dict.items()):
                 if value != 0:
                     print('{}: {}'.format(key, value))
-
-except Exception as err:
-    pass
 
 finally:
     print('File size: {}'.format(size_total))
