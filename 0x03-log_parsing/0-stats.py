3e82b4be0eceaddff699dc46c91f8b965442bfa6
ismail-la <lahbariismail@gmail.com>
2024-01-24 00:54:04 +0100

    0x03. Log Parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -22,36 +22,35 @@ Write a script that reads stdin line by line and computes metrics:
 
 import sys
 
-codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
-              '404': 0, '405': 0, '500': 0}
+if __name__ == '__main__':
 
-size_total = 0
-count = 0
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
 
-try:
-    for line in sys.stdin:
-        line_list = line.split(" ")
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(filesize))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
 
-        if len(line_list) > 4:
-            code_status = line_list[-2]
-            size_file = int(line_list[-1])
-
-            if code_status in codes_dict.keys():
-                codes_dict[code_status] += 1
-
-            size_total += size_file
+    try:
+        for line in sys.stdin:
             count += 1
-
-        if count == 10:
-            count = 0
-            print('File size: {}'.format(size_total))
-
-            for key, value in sorted(codes_dict.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
-
-finally:
-    print('File size: {}'.format(size_total))
-    for key, value in sorted(codes_dict.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                filesize += int(data[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
+    except KeyboardInterrupt:
+        print_stats(stats, filesize)
+        raise
