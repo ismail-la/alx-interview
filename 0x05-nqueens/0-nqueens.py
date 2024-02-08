e2405ba239994250bf586785f5ec157356de6310
ismail-la <lahbariismail@gmail.com>
2024-02-08 19:17:39 +0100

    0x05. N Queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,68 +1,40 @@
-#!/usr/bin/python3
-"""N Queens problem Solution module"""
-
 import sys
 
+def is_not_under_attack(queens_pos, row, col):
+    for queen_row, queen_col in queens_pos:
+        if queen_row == row or queen_col == col or queen_row + queen_col == row + col or queen_row - queen_col == row - col:
+            return False
+    return True
 
-def Get_solutions(ro, col):
-    resolution = [[]]
-    for queen in range(ro):
-        resolution = queen_position(queen, col, resolution)
-    return resolution
+def solve_nqueens(n, row, queens_pos, solutions):
+    if row == n:
+        solutions.append(queens_pos[:])  # Found a valid solution
+        return
+    for col in range(n):
+        if is_not_under_attack(queens_pos, row, col):
+            queens_pos.append((row, col))
+            solve_nqueens(n, row + 1, queens_pos, solutions)
+            queens_pos.pop()
 
-
-def queen_position(queen, col, prev_resolution):
-    safe_place = []
-    for array in prev_resolution:
-        for i in range(col):
-            if is_queen_placement_safe(queen, i, array):
-                safe_place.append(array + [i])
-    return safe_place
-
-
-def is_queen_placement_safe(q, i, array):
-    """checks if placing a queen.
-    -q:  is likely the column index we're trying to check for safety.
-    -i: is the row index we're considering placing the queen in.
-    -array: represents the current arrangement of queens on the board.
-    """
-    if i in array:
-        return (False)
-    else:
-        return all(abs(array[column] - i) != q - column
-                   for column in range(q))
-
-
-def init():
-    """ initialization of the program
-    The function checks if the number of command-line arguments provided
-    is exactly 2. If not, it prints a usage message and exits the program
-    with an error code of 1.
-    """
+def main():
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    if sys.argv[1].isdigit():
+    try:
         N = int(sys.argv[1])
-    else:
+    except ValueError:
         print("N must be a number")
         sys.exit(1)
     if N < 4:
         print("N must be at least 4")
         sys.exit(1)
-    return (N)
 
+    solutions = []
+    solve_nqueens(N, 0, [], solutions)
 
-def Nqueens():
+    for solution in solutions:
+        print(solution)
 
-    N = init()
-    solutions = Get_solutions(N, N)
-    for array in solutions:
-        clean = []
-        for q, i in enumerate(array):
-            clean.append([q, i])
-        print(clean)
+if __name__ == "__main__":
+    main()
 
-
-if __name__ == '__main__':
-    Nqueens()
