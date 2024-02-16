fc096b10e5093c50c8df24a388c2cec0196eea39
ismail-la <lahbariismail@gmail.com>
2024-02-16 03:07:58 +0100

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,68 +1,31 @@
 #!/usr/bin/node
 
-'use strict';
-
 const request = require('request');
 
-/**
- * Function to make a request to the Star Wars API to retrieve movie information
- * and print the characters of the movie based on the provided Movie ID.
- * @param {string} movieId - The ID of the Star Wars movie.
- */
-function getMovieCharacters(movieId) {
-  const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const movieId = process.argv[2];
+const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-  request.get(movieUrl, (error, response, body) => {
+function sendRequest (characterList, index) {
+  if (characterList.length === index) {
+    return;
+  }
+
+  request(characterList[index], (error, response, body) => {
     if (error) {
-      console.error('Error:', error);
-      return;
-    }
-
-    if (response.statusCode !== 200) {
-      console.error('Invalid response:', response.statusCode);
-      return;
-    }
-
-    try {
-      const movieData = JSON.parse(body);
-      const characters = movieData.characters;
-
-      if (!characters || characters.length === 0) {
-        console.log('No characters found for this movie.');
-        return;
-      }
-
-      // Sequentially print characters
-      characters.forEach(characterUrl => {
-        request.get(characterUrl, (error, response, body) => {
-          if (error) {
-            console.error('Error:', error);
-            return;
-          }
-
-          if (response.statusCode !== 200) {
-            console.error('Invalid response:', response.statusCode);
-            return;
-          }
-
-          const characterData = JSON.parse(body);
-          console.log(characterData.name);
-        });
-      });
-    } catch (parseError) {
-      console.error('Error parsing response:', parseError);
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      sendRequest(characterList, index + 1);
     }
   });
 }
 
-// Get the Movie ID from the command line arguments
-const args = process.argv.slice(2);
-const movieId = args[0];
+request(movieEndpoint, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const characterList = JSON.parse(body).characters;
 
-if (!movieId) {
-  console.error('Please provide the Movie ID as the first argument.');
-  process.exit(1);
-}
-
-getMovieCharacters(movieId);
-
+    sendRequest(characterList, 0);
+  }
+});
