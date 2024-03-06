1ba059e60e0c05e4604a6dc3191c8fdc13269635
ismail-la <lahbariismail@gmail.com>
2024-03-06 17:15:23 +0100

    0x09. Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-""" Island Perimeter problem """
+""" Island perimeter problem
+"""
 
 
 def island_perimeter(grid):
-    """ Calculates the perimeter of an island"""
+    """ Calculates perimeter of an island
+    """
     perimeter = 0
-    for i in range(len(grid)):
+    for row_index in range(len(grid)):
         for cell_index in range(len(grid[0])):
-            cell_current = grid[i][cell_index]
-            if not cell_current:
+            current_cell = grid[row_index][cell_index]
+            if not current_cell:
                 continue
-            cell_upper = None if not i else \
-                grid[i - 1][cell_index]
+            upper_cell = None if not row_index else \
+                grid[row_index - 1][cell_index]
             left_cell = None if not cell_index else \
                 grid[row_index][cell_index - 1]
             cell_perimeter = 4
-            if cell_upper:
+            if upper_cell:
                 cell_perimeter = cell_perimeter - 2
             if left_cell:
                 cell_perimeter = cell_perimeter - 2
