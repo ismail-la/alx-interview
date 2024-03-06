6d9f31869d7824a64eec6d133148a79e88043140
ismail-la <lahbariismail@gmail.com>
2024-03-06 17:19:36 +0100

    0x09. Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,15 +7,15 @@ def island_perimeter(grid):
     perimeter = 0
     for i in range(len(grid)):
         for cell_index in range(len(grid[0])):
-            current_cell = grid[i][cell_index]
-            if not current_cell:
+            cell_current = grid[i][cell_index]
+            if not cell_current:
                 continue
-            upper_cell = None if not i else \
+            cell_upper = None if not i else \
                 grid[i - 1][cell_index]
             left_cell = None if not cell_index else \
                 grid[i][cell_index - 1]
             cell_perimeter = 4
-            if upper_cell:
+            if cell_upper:
                 cell_perimeter = cell_perimeter - 2
             if left_cell:
                 cell_perimeter = cell_perimeter - 2
