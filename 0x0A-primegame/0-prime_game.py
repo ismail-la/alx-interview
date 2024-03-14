fc26b212d8d97c104d5cbe4917b3a42585446130
ismail-la <lahbariismail@gmail.com>
2024-03-14 15:48:07 +0000

    0x0A. Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,37 +1,33 @@
 #!/usr/bin/python3
-""" Module for solving prime game problem using the eratosthenes algorithm.
+""" Prime Game Problem
+    Topic: Eratosthenes algorithm
 """
 
 
 def isWinner(x, nums):
-    """Determine the winner in the prime game.
-    x: An integer representing the number of rounds in the prime game.
-    nums: A list of integers representing the upper bounds for each round.
     """
-    Maria = 0
+    Determines the winner in the prime game using
+    Eratosthenes prime sieving algorithm
+    """
     Ben = 0
-
+    Maria = 0
 
     for round in range(x):
-        """his loop iterates through each round of the game (from 0 to x-1).
-        """
-        List_playing_numbers = [num for num in range(2, nums[round] + 1)]
+        playing_numbers = [num for num in range(2, nums[round] + 1)]
         index = 0
-        # Implements the Sieve of Eratosthenes algorithm
-        while (index < len(List_playing_numbers)):
-            currentPrime = List_playing_numbers[index]
-            sieveIndex = index + currentPrime
-            while(sieveIndex < len(List_playing_numbers)):
-                List_playing_numbers.pop(sieveIndex)
-                sieveIndex += currentPrime - 1
+        # Sieve prime numbers per round
+        while (index < len(playing_numbers)):
+            current_prime = playing_numbers[index]
+            sieve_index = index + current_prime
+            while(sieve_index < len(playing_numbers)):
+                playing_numbers.pop(sieve_index)
+                sieve_index += current_prime - 1
             index += 1
-        # Determining the Winner:
-        # -If the count is even, Maria wins (increment Maria),
-        # Otherwise, Ben wins (increment Ben).
-        # -If Maria and Ben have the same score, the function returns None.
-        # Otherwise, it returns the name of the winner either ‘Ben’ or ‘Maria’
-        primeCount = (len(List_playing_numbers))
-        if primeCount and primeCount % 2:
+        # Determine winner - if number of primes is even player 1 wins
+        # else player 2 wins. Player 2  also wins if there is only one
+        # number to pick from
+        prime_count = (len(playing_numbers))
+        if prime_count and prime_count % 2:
             Maria += 1
         else:
             Ben += 1
